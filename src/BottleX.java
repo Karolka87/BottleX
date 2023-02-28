9d022cee143c20d513ef08d1c62a7d2f35027167
Karolka87 <115976221+Karolka87@users.noreply.github.com>
2023-02-28 13:06:50 +0100

    version not-finished

---
+++ src/BottleX.java
@@ -0,0 +1,53 @@
+public class BottleX {
+    public static void main(String[] args){
+        Bottle[] bottles = new Bottle[33];
+        for (int i = 0; i < bottles.length; i++) {
+            for (double j = 2.5 ;j < 200; j++)
+            bottles[i] = new Bottle(j);
+        }
+
+        bottles[0].pour(2.5);
+        bottles[1].pour(3);
+        bottles[0].transfer(0.5, bottles[1]);
+        System.out.println(bottles[1].getCurrentState());
+        System.out.println(bottles[0].getCurrentState());
+
+       /* for (int i = 0; i < bottles.length; i++){
+            Bottle[i] bottles = new Bottle[];*/
+    }
+}
+
+class Bottle {
+    double bottleCapacity;
+    //int bottleNumber;
+    private double currentState;
+    Bottle(double bottleCapacity) {
+        this.bottleCapacity = bottleCapacity;
+    }
+    double getBottleCapacity() {
+        return bottleCapacity;
+    }
+    double getCurrentState() {
+        return currentState;
+    }
+
+    double pour(double howManyLiteres) {
+        if ((this.currentState + howManyLiteres) <= bottleCapacity) {
+            this.currentState += howManyLiteres;
+            return howManyLiteres;
+        } else
+            return 0;
+
+    }
+    double pourOut(double howManyLiteres) {
+        if (this.currentState >= bottleCapacity) {
+            this.currentState -= bottleCapacity;
+            return bottleCapacity;
+        } else
+            return 0;
+    }
+    void transfer(double howManyLiteres, Bottle whereToTranfser) {
+        this.pourOut(bottleCapacity);
+        whereToTranfser.pour(bottleCapacity);
+    }
+}
